ef869e086e628b44d632c199b9fc31ecc9561402
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-02-08 14:46:07 -0500

    theme alterations

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import initScrollReveal from "./scripts/scrollReveal";
-import initTiltEffect from "./scripts/tiltAnimation";
-import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+import initScrollReveal from "./scripts/scrollReveal.js";
+import initTiltEffect from "./scripts/tiltAnimation.js";
+import { targetElements, defaultProps } from "./data/scrollRevealConfig.js";
 
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
